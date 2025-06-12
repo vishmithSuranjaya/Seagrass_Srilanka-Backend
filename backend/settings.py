17d773732e4e6a19f462f6f51f95db2994e9b239
Wasana <nilakshiwasana885@gmail.com>
2025-06-12 11:54:41 +0530

    initial pull

--- backend/settings.py
+++ backend/settings.py
@@ -139,7 +139,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'media')
+#STATIC_ROOT = os.path.join(BASE_DIR, 'media')
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
@@ -184,7 +185,8 @@ CORS_ALLOW_CREDENTIALS = True
 
 #Media files
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+#MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = BASE_DIR / 'media'
 
 #file uploads
 FILE_UPLOAD_PERMISSIONS = 0o644
