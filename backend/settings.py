56b16a29fda4ebdef6ea320eed54ada2ae1b7f51
Wasana <nilakshiwasana885@gmail.com>
2025-06-13 12:41:21 +0530

    initial change

--- backend/settings.py
+++ backend/settings.py
@@ -139,12 +139,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
-<<<<<<< HEAD
 #STATIC_ROOT = os.path.join(BASE_DIR, 'media')
 STATIC_ROOT = BASE_DIR / 'staticfiles'
-=======
-# STATIC_ROOT = os.path.join(BASE_DIR, 'media')
->>>>>>> f6c577fae0f26ba066a17aa06a3c833c5bd679ee
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
