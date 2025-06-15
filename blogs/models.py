ac14f77d77140371ac4ef19a8d48bd6b2546f7b6
Wasana <nilakshiwasana885@gmail.com>
2025-06-15 21:02:44 +0530

    login test

--- blogs/models.py
+++ blogs/models.py
@@ -7,7 +7,7 @@ from users.models import Users
 class Blog(models.Model):
     #blog post model
     blog_id = models.CharField(max_length=10, primary_key=True)
-    user_id = models.ForeignKey(User, on_delete=models.CASCADE, to_field='user_id' )
+    user_id = models.ForeignKey(Users, on_delete=models.CASCADE, to_field='user_id' )
     like_count = models.IntegerField(default =0)
     title = models.CharField(max_length=200)
     content = models.TextField(max_length = 1000)
