1377bc4af5e2400c1b3684370d9e489e73ae4e91
vishmithSuranjaya <suranjaya0327@gmail.com>
2025-09-21 00:46:42 -0700

    product image seperation

--- products/serializers.py
+++ products/serializers.py
@@ -1,8 +1,17 @@
 from rest_framework import serializers
-from .models import Product, Cart, Payment, CartItem
+from .models import Product, Cart, Payment, CartItem, ProductImage
+
+
+class ProductImageSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ProductImage
+        fields = "__all__"
 
 
 class ProductSerializer(serializers.ModelSerializer):
+
+    images = ProductImageSerializer(many=True, read_only=True)  
+
     class Meta:
         model = Product
         fields = '__all__'
@@ -27,6 +36,7 @@ class ProductSerializer(serializers.ModelSerializer):
         return value
   
 
+
 class PaymentSerializer(serializers.ModelSerializer):
     orders = serializers.SerializerMethodField()
     
@@ -48,13 +58,18 @@ class CartItemSerializer(serializers.ModelSerializer):
     product_id = serializers.CharField(source="product.product_id", read_only=True)
     product_name = serializers.CharField(source="product.name", read_only=True)
     product_price = serializers.DecimalField(source="product.price", max_digits=20, decimal_places=2, read_only=True)
-    product_image = serializers.ImageField(source="product.image", read_only=True)
+    product_image = serializers.SerializerMethodField()
 
     class Meta:
         model = CartItem
-        fields = ["id", "product_id", "product_name", "product_price", "product_image", "count", "line_total"]
+        fields = '__all__'
 
-        
+    def get_product_image(self, obj):
+        first_image = obj.product.images.first()  # <-- use related_name "images"
+        if first_image:
+            return first_image.image.url
+        return None
+    
 class CartSerializer(serializers.ModelSerializer):
     items = CartItemSerializer(many=True, read_only=True)
 
